c4a3a1219d329d96e6739dd8a7a203a9cbd67dfe
Dmitrii Fediuk <dfediuk@gmail.com>
2024-05-08 02:42:31 +0100

    https://github.com/cabinetsbay/catalog/issues/1

---
+++ Category.php
@@ -0,0 +1,8 @@
+<?php
+namespace CabinetsBay\Catalog;
+# 2024-05-08 Dmitrii Fediuk https://upwork.com/fl/mage2pro
+# "Refactor the names of categories' custom attributes as constants":
+# https://github.com/cabinetsbay/catalog/issues/1
+final class Category {
+
+}
