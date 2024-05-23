e0cf0740e600b627d9f12141826345a69decce8b
Dmitrii Fediuk <dfediuk@gmail.com>
2024-05-23 03:00:23 +0100

    https://github.com/cabinetsbay/catalog/issues/27

---
+++ B/Featured.php
@@ -0,0 +1,11 @@
+<?php
+namespace CabinetsBay\Catalog\B;
+# 2024-05-23
+# "Refactor the «Popular RTA Cabinet Styles» home page block as a widget": https://github.com/cabinetsbay/catalog/issues/27
+final class Featured {
+	/**
+	 * 2024-05-23
+	 * @param int[] $categoryIds
+	 */
+	static function p(array $categoryIds, string $cmsBlockId):string {return '';}
+}
