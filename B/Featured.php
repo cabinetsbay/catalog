7b77a381c76d34f2ca57af6bfe1461869e01d142
Dmitrii Fediuk <dfediuk@gmail.com>
2024-05-23 03:15:38 +0100

    https://github.com/cabinetsbay/catalog/issues/27

--- B/Featured.php
+++ B/Featured.php
@@ -5,7 +5,10 @@ namespace CabinetsBay\Catalog\B;
 final class Featured {
 	/**
 	 * 2024-05-23
+	 * @used-by vendor/cabinetsbay/core/view/frontend/templates/home.phtml
 	 * @param int[] $categoryIds
 	 */
-	static function p(array $categoryIds, string $cmsBlockId):string {return df_block_output($c = __CLASS__, df_class_llc($c));}
+	static function p(array $categoryIds, string $cmsBlockId):string {return df_block_output(
+		$c = __CLASS__, df_class_llc($c), ['categoryIds' => $categoryIds, 'cmsBlockId' => $cmsBlockId]
+	);}
 }
