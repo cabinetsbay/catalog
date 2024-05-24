67abde9ec602a39b3169a4d4c14ced3b2d28079a
Dmitrii Fediuk <dfediuk@gmail.com>
2024-05-24 02:30:42 +0100

    https://github.com/cabinetsbay/catalog/issues/27

--- Plugin/Category/DataProvider.php
+++ Plugin/Category/DataProvider.php
@@ -12,7 +12,7 @@ final class DataProvider {
 	 * 2) https://github.com/magento/magento2/blob/2.4.7/app/code/Magento/Catalog/view/adminhtml/ui_component/category_form.xml#L156-L223
 	 * @see \Magento\Catalog\Model\Category\DataProvider::prepareMeta()
 	 */
-	function afterPrepareMeta(Sb $sb, array $r):array {return df_category_dp_meta($r, [
+	function afterPrepareMeta(Sb $sb, array $r):array {return df_category_dp_meta($sb, $r, [
 		A::ASSEMBLY, A::KITCHEN_COLOR, A::KITCHEN_PRICE, A::KITCHEN_SET, A::KITCHEN_STYLE, A::KITCHEN_TYPE,
 		A::MATCHING_PRODUCTS, A::SPECS, A::STYLES
 	]);}
