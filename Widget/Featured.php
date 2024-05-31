11eae7bb243810d718501aae7e7139f927c8dee1
Dmitrii Fediuk <dfediuk@gmail.com>
2024-05-31 15:09:46 +0100

    https://github.com/cabinetsbay/catalog/issues/27

--- Widget/Featured.php
+++ Widget/Featured.php
@@ -5,4 +5,11 @@ use Magento\Widget\Block\BlockInterface as IWBlock;
 # 2024-05-31
 # "Refactor the «Popular RTA Cabinet Styles» home page block as a widget" https://github.com/cabinetsbay/catalog/issues/27
 /** @final Unable to use the PHP «final» keyword here because of the M2 code generation. */
-class Featured {}
+class Featured implements IIdentity {
+	/**
+	 * 2024-05-31
+	 * @override
+	 * @see \Magento\Framework\DataObject\IdentityInterface::getIdentities()
+	 */
+	function getIdentities() {return [];}
+}
