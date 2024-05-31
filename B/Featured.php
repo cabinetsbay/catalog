7daaafee260922777776ad3662a2cd3039ce4f41
Dmitrii Fediuk <dfediuk@gmail.com>
2024-05-31 17:23:15 +0100

    https://github.com/cabinetsbay/catalog/issues/27

--- B/Featured.php
+++ B/Featured.php
@@ -6,6 +6,7 @@ final class Featured {
 	/**
 	 * 2024-05-23
 	 * @used-by vendor/cabinetsbay/core/view/frontend/templates/home.phtml
+	 * @used-by \CabinetsBay\Catalog\Widget\Featured::_toHtml()
 	 * @param int[] $categoryIds
 	 */
 	static function p(array $categoryIds, string $cmsBlockId):string {return df_block_output(
