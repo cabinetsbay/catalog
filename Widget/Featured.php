9bf227fdb0e055494e0a21487ae09d0d8af17120
Dmitrii Fediuk <dfediuk@gmail.com>
2024-05-31 16:15:02 +0100

    https://github.com/cabinetsbay/catalog/issues/32

--- Widget/Featured.php
+++ Widget/Featured.php
@@ -1,24 +1,37 @@
 <?php
 namespace CabinetsBay\Catalog\Widget;
 use Magento\Framework\DataObject\IdentityInterface as IIdentity;
+use Magento\Framework\View\Element\AbstractBlock;
 use Magento\Framework\View\Element\BlockInterface as IBlock;
 use Magento\Widget\Block\BlockInterface as IWBlock;
 # "Refactor the «Popular RTA Cabinet Styles» home page block as a widget" https://github.com/cabinetsbay/catalog/issues/27
 /** @final Unable to use the PHP «final» keyword here because of the M2 code generation. */
-class Featured implements
+class Featured
 	/**
 	 * 2024-05-31
-	 * 1) «CabinetsBay\Catalog\Widget\Featured does not implement BlockInterface»:
+	 * 1.1) «Invalid block type: CabinetsBay\Catalog\Widget\Featured»:
+	 * https://github.com/cabinetsbay/catalog/issues/32
+	 * 1.2) @see \Magento\Framework\View\Layout\Generator\Block::getBlockInstance():
+	 * 		if (!$block instanceof \Magento\Framework\View\Element\AbstractBlock) {
+	 * 			throw new LocalizedException(
+	 * 				new \Magento\Framework\Phrase(
+	 * 					'Invalid block type: %1',
+	 * 					[is_object($block) ? get_class($block) : (string) $block]
+	 * 				),
+	 * 				$e
+	 * 			);
+	 * 		}
+	 * https://github.com/magento/magento2/blob/2.4.7/lib/internal/Magento/Framework/View/Layout/Generator/Block.php#L277-L285
+	 * 2.1) «CabinetsBay\Catalog\Widget\Featured does not implement BlockInterface»:
 	 * https://github.com/cabinetsbay/catalog/issues/31
-	 * 2) @see \Magento\Framework\View\Element\BlockFactory::createBlock():
+	 * 2.2) @see \Magento\Framework\View\Element\BlockFactory::createBlock():
 	 * 		if (!$block instanceof BlockInterface) {
 	 * 			throw new \LogicException($blockName . ' does not implement BlockInterface');
 	 * 		}
 	 * https://github.com/magento/magento2/blob/2.4.7/lib/internal/Magento/Framework/View/Element/BlockFactory.php#L45-L47
 	 */
-	IBlock
-	,IIdentity
-	,IWBlock
+	extends AbstractBlock
+	implements IIdentity, IWBlock
 {
 	/**
 	 * 2024-05-31
@@ -47,7 +60,7 @@ class Featured implements
 	/**
 	 * 2024-05-31
 	 * @override
-	 * @see \Magento\Framework\View\Element\BlockInterface::toHtml()
+	 * @see \Magento\Framework\View\Element\AbstractBlock::toHtml()
 	 */
 	function toHtml():string {return __METHOD__;}
 }
