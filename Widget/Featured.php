79dd66bb4be0435c0cf431d9a9f49191cbfb8046
Dmitrii Fediuk <dfediuk@gmail.com>
2024-05-31 16:06:26 +0100

    https://github.com/cabinetsbay/catalog/issues/31

--- Widget/Featured.php
+++ Widget/Featured.php
@@ -1,11 +1,25 @@
 <?php
 namespace CabinetsBay\Catalog\Widget;
 use Magento\Framework\DataObject\IdentityInterface as IIdentity;
+use Magento\Framework\View\Element\BlockInterface as IBlock;
 use Magento\Widget\Block\BlockInterface as IWBlock;
-# 2024-05-31
 # "Refactor the «Popular RTA Cabinet Styles» home page block as a widget" https://github.com/cabinetsbay/catalog/issues/27
 /** @final Unable to use the PHP «final» keyword here because of the M2 code generation. */
-class Featured implements IIdentity, IWBlock {
+class Featured implements
+	/**
+	 * 2024-05-31
+	 * 1) «CabinetsBay\Catalog\Widget\Featured does not implement BlockInterface»:
+	 * https://github.com/cabinetsbay/catalog/issues/31
+	 * 2) @see \Magento\Framework\View\Element\BlockFactory::createBlock():
+	 * 		if (!$block instanceof BlockInterface) {
+	 * 			throw new \LogicException($blockName . ' does not implement BlockInterface');
+	 * 		}
+	 * https://github.com/magento/magento2/blob/2.4.7/lib/internal/Magento/Framework/View/Element/BlockFactory.php#L45-L47
+	 */
+	IBlock
+	,IIdentity
+	,IWBlock
+{
 	/**
 	 * 2024-05-31
 	 * @override
@@ -29,4 +43,11 @@ class Featured implements IIdentity, IWBlock {
 	 * @param mixed $v [optional]
 	 */
 	function setData($k, $v = null):self {return $this;}
+
+	/**
+	 * 2024-05-31
+	 * @override
+	 * @see \Magento\Framework\View\Element\BlockInterface::toHtml()
+	 */
+	function toHtml():string {return __METHOD__;}
 }
