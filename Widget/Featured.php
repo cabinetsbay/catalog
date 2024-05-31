bc20d8349945c6e42a4b9c6ec722c866f1828d3e
Dmitrii Fediuk <dfediuk@gmail.com>
2024-05-31 15:08:14 +0100

    https://github.com/cabinetsbay/catalog/issues/27

--- Widget/Featured.php
+++ Widget/Featured.php
@@ -1,5 +1,7 @@
 <?php
 namespace CabinetsBay\Catalog\Widget;
+use Magento\Framework\DataObject\IdentityInterface as IIdentity;
+use Magento\Widget\Block\BlockInterface as IWBlock;
 # 2024-05-31
 # "Refactor the «Popular RTA Cabinet Styles» home page block as a widget" https://github.com/cabinetsbay/catalog/issues/27
 /** @final Unable to use the PHP «final» keyword here because of the M2 code generation. */
