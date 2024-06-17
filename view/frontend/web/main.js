67fb8571926b53f15e4947420f1bd41242a9bbe7
Dmitrii Fediuk <dfediuk@gmail.com>
2024-06-17 16:15:14 +0100

    https://github.com/cabinetsbay/catalog/issues/39

--- view/frontend/web/main.js
+++ view/frontend/web/main.js
@@ -2,7 +2,19 @@
 // "The frontend product list pages should show products as a grid on narrow screens and as a list on wide screens":
 // https://github.com/cabinetsbay/catalog/issues/39
 define(['jquery', 'domReady!'], $ => {
-	if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-		$('.products.wrapper.list').removeClass('products-list').addClass('products-grid');
+	const $w = $(window);
+	const $c = $('.products', $('body.page-products'));
+	if ($c.length) {
+		// 2024-06-17 https://api.jquery.com/resize
+		$w.on('resize', () => {
+			let a = ['products-list', 'products-grid'];
+			// 2024-06-17
+			// 1.1) https://api.jquery.com/height
+			// 1.2) https://stackoverflow.com/a/7789096
+			// 2.1) https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
+			// 2.2) https://caniuse.com/mdn-javascript_operators_destructuring
+			const [add, remove] = 680 < $w.height() ? a : a.reverse();
+			$c.addClass(add).removeClass(remove);
+		});
 	}
 });
