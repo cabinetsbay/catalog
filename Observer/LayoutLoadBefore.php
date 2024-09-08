0f778caa5ae413dcdb773ca7d3cbd07e466df5a0
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-08 22:30:44 +0100

    https://github.com/cabinetsbay/catalog/issues/47

--- Observer/LayoutLoadBefore.php
+++ Observer/LayoutLoadBefore.php
@@ -17,7 +17,8 @@ final class LayoutLoadBefore implements ObserverInterface {
 	 * @used-by \Magento\Framework\Event\Invoker\InvokerDefault::_callObserverMethod()
 	 */
 	function execute(O $o):void {
-		if (df_is_catalog_product_list()) {
+		/** @var bool $l */
+		if ($l = df_is_catalog_product_list()) {
 			df_body_class(
 				'cb-category-level-' . df_category_level()
 				 # 2024-06-10
@@ -26,6 +27,15 @@ final class LayoutLoadBefore implements ObserverInterface {
 				 # 2) Level 5: https://localhost.com:2255/ready-to-assemble-cabinets/croydon-white-shaker/pantry-oven-cabinets/pantry-cabinets.html
 				,3 > df_category_level() ? '' : 'cb-category-level-ge3'
 			);
+		}
+		if ($l || df_is_catalog_product_view()) {
+			# Dmitrii Fediuk https://upwork.com/fl/mage2pro
+			# 1) "All frontend catalog pages should have the `1column` layout": https://github.com/cabinetsbay/catalog/issues/47
+			# 2) "The `<body>` tag of the frontend catalog pages should not have the `page-layout-2columns-left` CSS class":
+			# https://github.com/cabinetsbay/catalog/issues/46
+			# 3) "The `main` frontend container is shifted right on catalog pages after the Magento 2.4.4 → 2.4.7-p2 upgrade":
+			# https://github.com/cabinetsbay/catalog/issues/45
+			df_page_config()->setPageLayout('1column');
 		}
 	}
 }
